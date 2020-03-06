5fe0a859ee6555bd5bfa89c12263a816ba20a75b
Blutronics <toanbum2000@gmail.com>
2020-03-06 10:13:33 -0600

    I think we need at least these for the moving bullets

--- src/main/java/myhomework/simulation/bullet.java
+++ src/main/java/myhomework/simulation/bullet.java
@@ -5,10 +5,33 @@
  */
 package myhomework.simulation;
 
+import myhomework.physics.Point;
+import myhomework.physics.Ray;
+import myhomework.physics.Vector;
+
 /**
  *
  * @author samsung1
  */
 public class bullet {
-    
+
+    private Ray r;
+
+    public bullet(double startX, double startY, double dX, double dY) {
+        Vector v = new Vector(dX, dY);
+        double speed = v.length();
+        r = new Ray(new Point(startX, startY), v, speed);
+    }
+
+    public Ray getRay() {
+        return r;
+    }
+
+    public void setRay(Ray r) {
+        this.r = r;
+    }
+
+    public void move(double time) {
+        r = new Ray(r.endPoint(time), r.v, r.speed);
+    }
 }
